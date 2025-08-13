0780886e30bb119fe1ebe6fe3520c3a27ec63c78
Daegun Kim <blutics@gmail.com>
2025-08-14 05:30:33 +0900

    update

--- nvim/.config/nvim/test_code.lua
+++
@@ -1,9 +0,0 @@
-print("hello world")
-
-local lspconfig = require('lspconfig')
-local util = require('lspconfig.util')
-
-local workspace = vim.fn.getcwd()
-print(workspace)
-local result = util.root_pattern("pyproject.toml", ".git", ".python-version")()
-print(result)
