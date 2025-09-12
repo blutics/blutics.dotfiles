0ccbd54d311c8f47407baa9bb4fd858de9adab6d
Daegun <blutics@gmail.com>
2025-09-13 07:02:29 +0900

    update

--- nvim/.config/nvim/lua/config/keymaps/init.lua
+++ nvim/.config/nvim/lua/config/keymaps/init.lua
@@ -15,7 +15,7 @@ require("config.keymaps.plugins.trouble_key")
 -- 기본 키 매핑
 
 vim.keymap.set("n", "<C-\\>", "<Cmd>ToggleTerm<CR>", { noremap = true, silent = true })
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "To Normal Mode" })
+-- vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "To Normal Mode" })
 
 vim.keymap.set("n", "<leader>o", "<Cmd>Portal jumplist backward<Cr>", { desc = "Portal backward" })
 vim.keymap.set("n", "<leader>i", "<Cmd>Portal jumplist forward<Cr>", { desc = "Portal forward" })
@@ -23,10 +23,13 @@ vim.keymap.set("n", "<leader>i", "<Cmd>Portal jumplist forward<Cr>", { desc = "P
 local todo_custom = require("custom.todo_custom_comment")
 
 vim.keymap.set("n", "<leader>/t", function()
-  todo_custom.add_todo_comment("todo")
+	todo_custom.add_todo_comment("todo")
 end, { desc = "Add todo" })
 
 local aaat = require("custom.obsidian_custom_add_aliases_and_tags")
 
 vim.keymap.set("n", "<leader>bya", aaat.modify_aliases, { desc = "Modify aliases in frontmatter" })
 vim.keymap.set("n", "<leader>byt", aaat.modify_tags, { desc = "Append Yaml Tags" })
+
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { silent = true, noremap = ture }) -- Primeagen의 Tip!
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { silent = true, noremap = ture })
