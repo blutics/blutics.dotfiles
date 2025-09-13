c0c8498b9c0a587aef71aa833c3d85cf1c88adfa
Daegun <blutics@gmail.com>
2025-09-13 22:28:13 +0900

    update

--- bash/.config/bash/rc.d/00-path.sh
+++ bash/.config/bash/rc.d/00-path.sh
@@ -1,6 +1,6 @@
 
 # pnpm
-export PNPM_HOME="/home/blutics/.local/share/pnpm"
+export PNPM_HOME="${XDG_DATA_HOME:-$HOME/.local/share}/pnpm"
 case ":$PATH:" in
   *":$PNPM_HOME:"*) ;;
   *) export PATH="$PNPM_HOME:$PATH" ;;
